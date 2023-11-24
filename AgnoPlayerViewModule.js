a4f083345899d9ba514385d843ed115438e02d19
Hardik Mashru <harrymash2006@gmail.com>
2023-11-24 15:19:07 +0530

    add default value for adTag

--- AgnoPlayerViewModule.js
+++ AgnoPlayerViewModule.js
@@ -98,7 +98,7 @@ AgnoPlayerViewModule.defaultProps = {
     muteOnAutoplay: true,
     playerSkinColor: '#FFFFFF',
     posterURL: null,
-    adTag: null,
+    adTag: 'https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/ad_rule_samples&ciu_szs=300x250&ad_rule=1&impl=s&gdfp_req=1&env=vp&output=vmap&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ar%3Dpreonly&cmsid=496&vid=short_onecue&correlator=',
     showTestAd: false,
     showAds: false,
     playButtonBackgroundColor: '#0000FF',
